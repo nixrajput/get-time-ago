3d9e2c0837ee5ea81cd5621bc1bd153ffe3c53a6
Mejans <61360811+Mejans@users.noreply.github.com>
2021-08-22 20:49:19 +0200

    Create oc_msg.dart

---
+++ lib/messages/languages/oc_msg.dart
@@ -0,0 +1,40 @@
+import '../messages.dart';
+
+/// Occitan messages
+class FrenchMessages implements Messages {
+  @override
+  String prefixAgo() => 'fa';
+
+  @override
+  String prefixFromNow() => "d’aquí";
+
+  @override
+  String suffixAgo() => '';
+
+  @override
+  String suffixFromNow() => '';
+
+  @override
+  String secsAgo(int seconds) => '$seconds segondas';
+
+  @override
+  String minAgo(int minutes) => 'una minuta';
+
+  @override
+  String minsAgo(int minutes) => '$minutes minutas';
+
+  @override
+  String hourAgo(int minutes) => 'una ora';
+
+  @override
+  String hoursAgo(int hours) => '$hours oras';
+
+  @override
+  String dayAgo(int hours) => 'un jorn';
+
+  @override
+  String daysAgo(int days) => '$days jors';
+
+  @override
+  String wordSeparator() => ' ';
+}
