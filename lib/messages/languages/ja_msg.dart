e1055e9f5ad2f8da32b6d2325ad53017f9551d64
ywake <ywake@student.42tokyo.jp>
2021-08-22 11:27:06 +0900

    add japanese support

---
+++ lib/messages/languages/ja_msg.dart
@@ -0,0 +1,40 @@
+import '../messages.dart';
+
+/// Japanese Messages
+class JapaneseMessages implements Messages {
+  @override
+  String prefixAgo() => '';
+
+  @override
+  String prefixFromNow() => '';
+
+  @override
+  String suffixAgo() => '前';
+
+  @override
+  String suffixFromNow() => '今から';
+
+  @override
+  String secsAgo(int seconds) => '$seconds秒';
+
+  @override
+  String minAgo(int minutes) => '1分';
+
+  @override
+  String minsAgo(int minutes) => '$minutes分';
+
+  @override
+  String hourAgo(int minutes) => '1時間';
+
+  @override
+  String hoursAgo(int hours) => '$hours時間';
+
+  @override
+  String dayAgo(int hours) => '1日';
+
+  @override
+  String daysAgo(int days) => '$days日';
+
+  @override
+  String wordSeparator() => '';
+}
