fdbb32bac967ac00640ee7788e85e06ea15dd1ac
Mejans <61360811+Mejans@users.noreply.github.com>
2021-08-23 08:58:22 +0200

    Update get_time_ago.dart

--- lib/get_time_ago.dart
+++ lib/get_time_ago.dart
@@ -7,6 +7,7 @@ import './messages/languages/hi_msg.dart';
 import './messages/languages/pt_br_msg.dart';
 import './messages/languages/zh_cn_msg.dart';
 import './messages/languages/ja_msg.dart';
+import './messages/languages/oc_msg.dart';
 import './messages/messages.dart';
 
 class GetTimeAgo {
@@ -21,6 +22,7 @@ class GetTimeAgo {
     'br': PortugueseBrazilMessages(),
     'zh': SimplifiedChineseMessages(),
     'ja': JapaneseMessages(),
+    'oc': OccitanMessages(),    
   };
 
   ///
