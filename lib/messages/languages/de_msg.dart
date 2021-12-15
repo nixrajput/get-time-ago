7802a7d4c9d4fbaf35ba760a9a0421fc17c69bdf
Pascal Otto <otto.pascal.acc@mail.de>
2021-12-15 01:25:09 +0100

    add german language support

---
+++ lib/messages/languages/de_msg.dart
@@ -0,0 +1,40 @@
+import '../messages.dart';
+
+/// English Messages
+class GermanMessages implements Messages {
+  @override
+  String prefixAgo() => 'vor';
+
+  @override
+  String prefixFromNow() => 'in';
+
+  @override
+  String suffixAgo() => '';
+
+  @override
+  String suffixFromNow() => '';
+
+  @override
+  String secsAgo(int seconds) => '$seconds Sekunden';
+
+  @override
+  String minAgo(int minutes) => 'einer Minute';
+
+  @override
+  String minsAgo(int minutes) => '$minutes Minuten';
+
+  @override
+  String hourAgo(int minutes) => 'einer Stunde';
+
+  @override
+  String hoursAgo(int hours) => '$hours Stunden';
+
+  @override
+  String dayAgo(int hours) => 'einem Tag';
+
+  @override
+  String daysAgo(int days) => '$days Tagen';
+
+  @override
+  String wordSeparator() => ' ';
+}
