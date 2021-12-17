3f257ae8f17d4c960492c3efa34eb9cd4a21eb5f
WingCH <wingch.apps@gmail.com>
2021-12-17 10:52:05 +0800

    add Traditional Chinese

---
+++ lib/messages/languages/zh_tw_msg.dart
@@ -0,0 +1,40 @@
+import '../messages.dart';
+
+/// Traditional Chinese Messages
+class TraditionalChineseMessages implements Messages {
+  @override
+  String prefixAgo() => '';
+
+  @override
+  String prefixFromNow() => '';
+
+  @override
+  String suffixAgo() => '前';
+
+  @override
+  String suffixFromNow() => '現在起';
+
+  @override
+  String secsAgo(int seconds) => '$seconds秒';
+
+  @override
+  String minAgo(int minutes) => '1分鐘';
+
+  @override
+  String minsAgo(int minutes) => '$minutes分鐘';
+
+  @override
+  String hourAgo(int minutes) => '1小時';
+
+  @override
+  String hoursAgo(int hours) => '$hours小時';
+
+  @override
+  String dayAgo(int hours) => '1天';
+
+  @override
+  String daysAgo(int days) => '$days天';
+
+  @override
+  String wordSeparator() => '';
+}
