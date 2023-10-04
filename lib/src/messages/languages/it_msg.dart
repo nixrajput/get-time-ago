1f753808f913a388f8974cccd5d4a6252f8b45e8
Patrick Nicolosi <patricknicolosi99@gmail.com>
2023-10-04 17:34:10 +0200

    Update it_msg.dart

--- lib/src/messages/languages/it_msg.dart
+++ lib/src/messages/languages/it_msg.dart
@@ -1,1 +1,31 @@
-
+class ItalianMessages implements Messages {
+  @override
+  String prefixAgo() => '';
+  
+  @override
+  String suffixAgo() => 'fa';
+  
+  @override
+  String secsAgo(int seconds) => '$seconds secondi';
+  
+  @override
+  String minAgo(int minutes) => 'un minuto';
+  
+  @override
+  String minsAgo(int minutes) => '$minutes minuti';
+  
+  @override
+  String hourAgo(int minutes) => 'un ora';
+  
+  @override
+  String hoursAgo(int hours) => '$hours ore';
+  
+  @override
+  String dayAgo(int hours) => 'un giorno';
+  
+  @override
+  String daysAgo(int days) => '$days giorni';
+  
+  @override
+  String wordSeparator() => ' ';
+}
